c2c44df4925c8600da21624af6ddb83a1fbde2ae
Philipp Palutin <RussDragon9000@gmail.com>
2020-05-05 02:12:33 +0300

    ObjectSearchStrategy.cs: Added better input validation and conducted a little refactoring

--- ObjectSearchStrategy.cs
+++ ObjectSearchStrategy.cs
@@ -3,14 +3,17 @@ using System.Threading.Tasks;
 
 namespace SCP
 {
-  public class SearchStrategy : IStrategy
+  public class ObjectSearchStrategy : IStrategy
   {
     private readonly ScpLibrary _library;
     
-    public SearchStrategy(ScpLibrary lib) => this._library = lib;
+    public ObjectSearchStrategy(ScpLibrary lib) => this._library = lib;
 
     public Task<string> GetText(string arg)
     {
+      if (string.IsNullOrEmpty(arg))
+        return Task.FromResult("Please, specify object number.");
+      
       var (link, title) = this._library.GetObjectLink(arg);
       if (string.IsNullOrWhiteSpace(link))
         return Task.FromResult("Sorry, I don't know such SCP");
